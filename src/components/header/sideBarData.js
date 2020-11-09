020a317d6ccfed0fef33ab87fe3f12e10c42adf2
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2020-11-09 22:02:39 +0600

    created responsive navbar with mui

---
+++ src/components/header/sideBarData.js
@@ -0,0 +1,27 @@
+export const SidebarData = [
+  {
+    title: "Home",
+    // path: "/",
+    // // icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Shop",
+    // path: "/reports",
+    // // icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+  },
+
+  {
+    title: "Contact",
+    // path: "/reports",
+    // // icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+  },
+  {
+    title: "Login",
+    // path: "/reports",
+    // // icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+  },
+];
