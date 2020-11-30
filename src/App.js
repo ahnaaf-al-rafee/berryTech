dab48f7a91de590246fd2e0752e655cf4a870c05
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2020-11-30 20:47:16 +0600

    added shop page styling not satisfiying

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Container } from "@material-ui/core";
 import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
+
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
-        <Container>
-          <Route exact path="/" component={HomePage} />
-        </Container>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
