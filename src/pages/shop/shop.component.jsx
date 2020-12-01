27906c65c520a22444877e180dec1a1500182f76
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2020-12-01 22:47:23 +0600

    updated shop page styling and shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 import SHOP_DATA from "./shop_data";
+
+import "./shop.styles.css";
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -12,7 +15,7 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const {collections} = this.state;
+    const { collections } = this.state;
     return (
       <div className="shop-page">
         {collections.map(({ id, ...otherProps }) => (
--- src/pages/shop/shop_data.js
+++ src/pages/shop/shop_data.js
@@ -1,250 +1,214 @@
 const SHOP_DATA = [
-    {
-      id: 1,
-      title: 'Hats',
-      routeName: 'hats',
-      items: [
-        {
-          id: 1,
-          name: 'Brown Brim',
-          imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-          price: 25
-        },
-        {
-          id: 2,
-          name: 'Blue Beanie',
-          imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-          price: 18
-        },
-        {
-          id: 3,
-          name: 'Brown Cowboy',
-          imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-          price: 35
-        },
-        {
-          id: 4,
-          name: 'Grey Brim',
-          imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-          price: 25
-        },
-        {
-          id: 5,
-          name: 'Green Beanie',
-          imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-          price: 18
-        },
-        {
-          id: 6,
-          name: 'Palm Tree Cap',
-          imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-          price: 14
-        },
-        {
-          id: 7,
-          name: 'Red Beanie',
-          imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-          price: 18
-        },
-        {
-          id: 8,
-          name: 'Wolf Cap',
-          imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-          price: 14
-        },
-        {
-          id: 9,
-          name: 'Blue Snapback',
-          imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-          price: 16
-        }
-      ]
-    },
-    {
-      id: 2,
-      title: 'Sneakers',
-      routeName: 'sneakers',
-      items: [
-        {
-          id: 1,
-          name: 'Adidas NMD',
-          imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-          price: 220
-        },
-        {
-          id: 2,
-          name: 'Adidas Yeezy',
-          imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-          price: 280
-        },
-        {
-          id: 3,
-          name: 'Black Converse',
-          imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-          price: 110
-        },
-        {
-          id: 4,
-          name: 'Nike White AirForce',
-          imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-          price: 160
-        },
-        {
-          id: 5,
-          name: 'Nike Red High Tops',
-          imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-          price: 160
-        },
-        {
-          id: 6,
-          name: 'Nike Brown High Tops',
-          imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-          price: 160
-        },
-        {
-          id: 7,
-          name: 'Air Jordan Limited',
-          imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-          price: 190
-        },
-        {
-          id: 8,
-          name: 'Timberlands',
-          imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-          price: 200
-        }
-      ]
-    },
-    {
-      id: 3,
-      title: 'Jackets',
-      routeName: 'jackets',
-      items: [
-        {
-          id: 1,
-          name: 'Black Jean Shearling',
-          imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-          price: 125
-        },
-        {
-          id: 2,
-          name: 'Blue Jean Jacket',
-          imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-          price: 90
-        },
-        {
-          id: 3,
-          name: 'Grey Jean Jacket',
-          imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-          price: 90
-        },
-        {
-          id: 4,
-          name: 'Brown Shearling',
-          imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-          price: 165
-        },
-        {
-          id: 5,
-          name: 'Tan Trench',
-          imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-          price: 185
-        }
-      ]
-    },
-    {
-      id: 4,
-      title: 'Womens',
-      routeName: 'womens',
-      items: [
-        {
-          id: 1,
-          name: 'Blue Tanktop',
-          imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-          price: 25
-        },
-        {
-          id: 2,
-          name: 'Floral Blouse',
-          imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-          price: 20
-        },
-        {
-          id: 3,
-          name: 'Floral Dress',
-          imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-          price: 80
-        },
-        {
-          id: 4,
-          name: 'Red Dots Dress',
-          imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-          price: 80
-        },
-        {
-          id: 5,
-          name: 'Striped Sweater',
-          imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-          price: 45
-        },
-        {
-          id: 6,
-          name: 'Yellow Track Suit',
-          imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-          price: 135
-        },
-        {
-          id: 7,
-          name: 'White Blouse',
-          imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-          price: 20
-        }
-      ]
-    },
-    {
-      id: 5,
-      title: 'Mens',
-      routeName: 'mens',
-      items: [
-        {
-          id: 1,
-          name: 'Camo Down Vest',
-          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-          price: 325
-        },
-        {
-          id: 2,
-          name: 'Floral T-shirt',
-          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-          price: 20
-        },
-        {
-          id: 3,
-          name: 'Black & White Longsleeve',
-          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-          price: 25
-        },
-        {
-          id: 4,
-          name: 'Pink T-shirt',
-          imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-          price: 25
-        },
-        {
-          id: 5,
-          name: 'Jean Long Sleeve',
-          imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-          price: 40
-        },
-        {
-          id: 6,
-          name: 'Burgundy T-shirt',
-          imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-          price: 25
-        }
-      ]
-    }
-  ]
+  {
+    id: 1,
+    title: "Accessories",
+    routeName: "accessories",
+    items: [
+      {
+        id: 1,
+        name: "Bose QuietComfort 35",
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/41jSuUHT8eL._AC_US160_.jpg",
+        price: 199.0,
+      },
+      {
+        id: 2,
+        name: "Apple EarPods with Lightning Connector - White",
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/21zodo7QkUL._AC_US160_.jpg",
+        price: 15,
+      },
+      {
+        id: 3,
+        name: "TP-Link AC1750 Smart WiFi Router",
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/415vAIn9uEL._AC_US160_.jpg",
+        price: 59.99,
+      },
+      {
+        id: 4,
+        name: "AMD Ryzen 7 3700X 8-Core, 16-Thread Unlocked Desktop Processor",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/717Di3DGIbL._AC_UY218_.jpg",
+        price: 304,
+      },
+      {
+        id: 5,
+        name: "Samsung Galaxy Buds+ Plus",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/51xvUyKNLCL._AC_UY218_.jpg",
+        price: 119,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "mobiles",
+    routeName: "mobiles",
+    items: [
+      {
+        id: 1,
+        name: "Apple iPhone 11 Pro",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/61vflt1U5gL._AC_UY218_.jpg",
+        price: 779,
+      },
+      {
+        id: 2,
+        name: "OnePlus 8 Pro Onyx Black",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/51az5uPx4AL._AC_UY218_.jpg",
+        price: 799.99,
+      },
+      {
+        id: 3,
+        name: "Samsung Galaxy S20",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/61nSb8Jn7oL._AC_UY218_.jpg",
+        price: 799,
+      },
+      {
+        id: 4,
+        name: "Realme C11",
+        imageUrl:
+          "https://static-01.daraz.com.bd/p/e162bb2eaae5ee208478ea39062c0d35.jpg",
+        price: 105.11,
+      },
+      {
+        id: 5,
+        name: "HUAWEI Y9",
+        imageUrl:
+          "https://static-01.daraz.com.bd/p/a9e9312e9cb0db020478571fb4d01412.jpg",
+        price: 253.89,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "softwares",
+    routeName: "softwares",
+    items: [
+      {
+        id: 1,
+        name: "ESET 2020 Internet Security",
+        imageUrl:
+          "https://static-01.daraz.com.bd/p/141443ce1cd10cdb7687177726d8fedb.jpg",
+        price: 5.9,
+      },
+      {
+        id: 2,
+        name: "Panda Antivirus and internet security",
+        imageUrl:
+          "https://static-01.daraz.com.bd/p/41a7b02c211d6f78ae642339958b5951.jpg",
+        price: 3.78,
+      },
+      {
+        id: 3,
+        name: "Kaspersky Internet Security 2019",
+        imageUrl:
+          "https://static-01.daraz.com.bd/p/82952f4ca6f22d846010431c423fde88.jpg",
+        price: 14.76,
+      },
+      {
+        id: 4,
+        name: "Microsoft Windows 10 Home",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/61-NY4+51BL._AC_UY218_.jpg",
+        price: 129,
+      },
+      {
+        id: 5,
+        name: "Adobe Acrobat Pro 2020",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/612VjwYnpnL._AC_UY218_.jpg",
+        price: 118,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "pc",
+    routeName: "pc",
+    items: [
+      {
+        id: 1,
+        name: "Acer Aspire C24-963-UA91",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/61PlVVOd3QL._AC_UY218_.jpg",
+        price: 579.99,
+      },
+      {
+        id: 2,
+        name: "iBUYPOWER Gaming PC",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/712irbqWpeL._AC_UY218_.jpg",
+        price: 529.99,
+      },
+      {
+        id: 3,
+        name: "HP 22-inch All-in-One Desktop",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71RLwsjsGcL._AC_UY218_.jpg",
+        price: 416.01,
+      },
+      {
+        id: 4,
+        name: "ASUS AiO",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71tk2rkxmPL._AC_UY218_.jpg",
+        price: 519,
+      },
+      {
+        id: 5,
+        name: "Latest_DELL Inspiron",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/31DX45GE4RL._AC_UY218_.jpg",
+        price: 620.0,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "mac",
+    routeName: "mac",
+    items: [
+      {
+        id: 1,
+        name: "New Apple MacBook Pro",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71BMVOs2xML._AC_UY218_.jpg",
+        price: 1294,
+      },
+      {
+        id: 2,
+        name: "Apple 13in MacBook Pro",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71nM55mRvxL._AC_UY218_.jpg",
+        price: 2199,
+      },
+      {
+        id: 3,
+        name: "Apple MacBook Pro 13in",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/51jckVG7HeL._AC_UY218_.jpg",
+        price: 749.99,
+      },
+      {
+        id: 4,
+        name: "New Apple iMac",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71KR2i6-WaL._AC_UY218_.jpg",
+        price: 1709,
+      },
+      {
+        id: 5,
+        name: " Apple iMac 21.5in",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/41HuYIKqsIL._AC_UY218_.jpg",
+        price: 749,
+      },
+    ],
+  },
+];
 
-  export default SHOP_DATA;
-  
+export default SHOP_DATA;
